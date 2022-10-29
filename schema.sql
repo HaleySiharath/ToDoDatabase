6bd714215cb7c62467eba6bd15f0124f1c2b51e5
HaleySiharath <haley.siharath@gmail.com>
2022-10-29 18:44:40 -0400

    added tables and database

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS ProjectName;
-CREATE DATABASE ProjectName;
-USE ProjectName;
+DROP DATABASE IF EXISTS ToDoDatabase;
+CREATE DATABASE ToDoDatabase;
+USE ToDoDatabase;
 
 -- todo: create tables to store data
 
@@ -10,3 +10,26 @@ USE ProjectName;
 --     user_name varchar(255) NOT NULL UNIQUE,
 --     user_password varchar(255) NOT NULL
 -- );
+
+CREATE TABLE User(
+    user_id int NOT NULL AUTO_INCREMENT,
+    user_name VARCHAR(20) NOT NULL UNIQUE,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE List(
+    list_id int NOT NULL AUTO_INCREMENT,
+    user_id int NOT NULL,
+    title VARCHAR(20) NOT NULL,
+    PRIMARY KEY (list_id)
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+CREATE TABLE Comment(
+    comment_id int NOT NULL AUTO_INCREMENT,
+    task_id int NOT NULL,
+    content VARCHAR(50) NOT NULL,
+    order int NOT NULL,
+    PRIMARY KEY (comment_id)
+    FOREIGN KEY (task_id) REFERENCES Task(task_id)
+);
