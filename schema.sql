f5dc1e5329e03e7fdad69091b8fe5e06b8989756
HaleySiharath <haley.siharath@gmail.com>
2022-11-18 23:37:17 -0500

    Code for sprint 2

--- schema.sql
+++ schema.sql
@@ -11,12 +11,42 @@ USE ToDoDatabase;
 --     user_password varchar(255) NOT NULL
 -- );
 
+CREATE TABLE Admin_User (
+	admin_id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(20) NOT NULL,
+    email VARCHAR(20) NOT NULL,
+    project_id INT UNIQUE,
+    PRIMARY KEY (admin_id)
+);
+
+CREATE TABLE Project(
+	team_id INT NOT NULL,
+    project_id INT NOT NULL,
+    role VARCHAR(30) NOT NULL,
+    PRIMARY KEY (team_id, project_id),
+  --  FOREIGN KEY (team_id) REFERENCES User(team_id),
+	FOREIGN KEY (project_id) REFERENCES Admin_User(project_id)
+);
+SET FOREIGN_KEY_CHECKS=0;
+
 CREATE TABLE User (
     user_id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(20) NOT NULL UNIQUE,
     password VARCHAR(20) NOT NULL,
     email VARCHAR(20) NOT NULL,
-    PRIMARY KEY (user_id)
+    team_id INT,
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (team_id) REFERENCES Project(team_id)
+);
+
+
+
+CREATE TABLE Project_List(
+	user_id int NOT NULL UNIQUE,
+    project_id int NOT NULL,
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (project_id) REFERENCES Project(project_id)
 );
 
 CREATE TABLE List(
@@ -61,16 +91,37 @@ CREATE TABLE Subtask(
 );
 
 -- INSERTING DATA INTO DATABASE
-INSERT INTO User(username, password, email) -- userid is incremented auto
-VALUES  ('Tristan', 'secret password', 'telsener@uncc.edu'), -- 1 user_id
-        ('Haley', 'password123', 'hsiharat@uncc.edu'),       -- 2 user_id
-        ('Jason', 'PaSsWoRd456', 'jkhotsom@uncc.edu');       -- 3 user_id
+INSERT INTO User(username, password, email, team_id) -- userid is incremented auto
+VALUES  ('Tristan', 'secret password', 'telsener@uncc.edu', NULL), -- 1 user_id
+        ('Haley', 'password123', 'hsiharat@uncc.edu', NULL),       -- 2 user_id
+        ('Jason', 'PaSsWoRd456', 'jkhotsom@uncc.edu', NULL),       -- 3 user_id
+        ('Darwin', 'Whathappen', 'dardar@gmail.com', 1),		   -- 4 user_id
+        ('Goofy', 'Yonks', 'goofy@gmail.com', 1),		   		   -- 5 user_id
+        ('Daffy', 'ILikeCarrots', 'daffyDuck1@gmail.com', 1);	   -- 6 user_id
+        
 
+INSERT INTO Admin_User(username, password, email, project_id) -- userid is incremented auto
+VALUES  ('Mone', 'passcode176', 'mone@corpirate.com', 1);			-- 1 admin_user
+
+INSERT INTO Project(project_id, team_id, role)
+VALUES	(1, 1, 'backend team'),										-- Project 1 team 1
+		(1, 2, 'frontend team');									-- Project 1 team 2
+
+INSERT INTO Project_List(user_id, project_id)
+VALUES	(4, 1),
+		(5, 1),
+        (6, 1);
+        
+        
 INSERT INTO List(user_id, title, description)
 VALUES  (1, 'Schoolwork', 'Assignemtns and Projects fo class'), 				  -- 1 list_id
         (1, 'Chores', 'Things that I need to get done arround the House'),        -- 2 list_id
         (2, 'To-Do', 'All of the things that I need to get done today'),          -- 3 list_id
-        (3, 'UNCC-GameDevs', 'List of UNCC Game Devs Projects');                  -- 4 list_id
+        (3, 'UNCC-GameDevs', 'List of UNCC Game Devs Projects'),                  -- 4 list_id
+        (4, 'Backend tasks', 'Complete all of the backend for the website'),	  -- 5 list_id
+		(5, 'Backend tasks', 'Complete all of the backend for the website'),	  -- 6 list_id
+		(6, 'Frontend tasks', 'Complete all of the fronted for the website');	  -- 7 list_id
+  
 
 INSERT INTO Task(list_id, title, description, reminder, due_date, important, completed)
 VALUES 
@@ -93,7 +144,18 @@ VALUES
         -- 9 task_id
         (4,'Code for Game','complete the code for the game in time for the next game jam','2022-11-04','2022-11-05 11:59:59',TRUE,TRUE),
         -- 10 task_id
-        (4,'Level design for Game','complete the level design for the game in time for the next game jam','2022-11-05','2022-11-05 11:59:59',TRUE,TRUE);
+        (4,'Level design for Game','complete the level design for the game in time for the next game jam','2022-11-05','2022-11-05 11:59:59',TRUE,TRUE),
+
+		(5,'CRUD Post','Make the CRUD element for posts','2022-11-04','2022-11-05 11:59:59',FALSE,TRUE),
+        -- 11 task_id
+        (5,'Database','make the interworking database','2022-11-04','2022-11-05 11:59:59',TRUE,TRUE),
+        -- 12 task_id
+        (5,'Password concept','Allow for password access using salt and peper','2022-11-05','2022-11-05 11:59:59',TRUE,TRUE),
+		-- 13 task_id
+        (6,'Login page','Make the login page pretty','2022-11-04','2022-11-05 11:59:59',FALSE,TRUE),
+        -- 14 task_id
+        (6,'Design interface','Make a base sketch for the whole application','2022-11-04','2022-11-05 11:59:59',TRUE,TRUE);
+        -- 15 task_id
 
 INSERT INTO Subtask(task_id, priority, description, completed)
 VALUES  -- 1 subtask_id
@@ -112,32 +174,26 @@ VALUES  -- 1 comment_id
         (4, 'Meet Natalia there at 3:00 pm so that everything works out');
 
 
--- SELECT * FROM User;
--- SELECT * FROM List;
--- SELECT * FROM Task;
--- SELECT * FROM Subtask;
--- SELECT * FROM Comment;
-
--- Display all of user tables and how many tasks each table has
-SELECT U.username as username, L.title as list, COUNT(*) as taks_count_per_table
-FROM User U
-INNER JOIN List L ON (L.user_id = U.user_id)
-JOIN Task T ON (T.list_id = L.list_id)
-GROUP BY U.user_id, L.list_id
-ORDER BY U.user_id;
 
 
--- Display all subtasks of a given task
-SELECT L.title as list, T.title as task, S.subtask_id, S.description as subtask
-FROM List L 
-INNER JOIN Task T ON (L.list_id = T.list_id)
-INNER JOIN Subtask S ON (T.task_id = S.task_id)
-ORDER BY T.task_id;
+-- Display the list of users on a project and what roles they are assigned
+SELECT U.username as teamUser, P.project_id as projectId, P.role as projectRole
+FROM Project P
+INNER JOIN User U on (U.team_id = P.team_id)
+INNER JOIN Admin_User A on (A.project_id = P.project_id);
 
+-- Display the tasks of a user who is in project 1 and team 1
+SELECT P.project_id as Project, P.team_id as Team, T.title as Tasks
+FROM Project P
+INNER JOIN Project_List PL on (PL.project_id = P.project_id)
+INNER JOIN List L on (PL.user_id = L.user_id)
+INNER JOIN Task T on (T.list_id = L.list_id)
+ORDER BY P.project_id;
 
--- Display all completed task
-SELECT L.title as list, T.title as completed_task, T.completed
-FROM Task T
-INNER JOIN List L ON (L.list_id = T.list_id)
-WHERE T.completed = TRUE
-ORDER BY T.list_id;
+-- Display the number of people on a team
+SELECT P.project_id as Project, P.team_id as Team, COUNT(*)
+FROM Project P
+INNER JOIN User U ON (U.team_id = P.team_id)
+INNER JOIN Admin_User A ON (A.project_id = P.project_id)
+GROUP BY P.project_id
+ORDER BY P.project_id, U.team_id;
